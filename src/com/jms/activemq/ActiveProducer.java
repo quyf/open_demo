c57a4de06a0865a4817d96ba2e436fffe128f26f
quyf <544529278@qq.com>
2015-08-18 15:02:03 +0800

    宿舍

---
+++ src/com/jms/activemq/ActiveProducer.java
@@ -0,0 +1,19 @@
+package com.jms.activemq;
+
+/**
+ *@author   quyf
+ *@version  1.0.0
+ *@date 	2015-8-18  下午3:00:05 
+ */
+
+public class ActiveProducer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
